edce67bc65f0a9e355724926bf12dc3f19588b41
Ratimon <ratixoxo@gmail.com>
2023-04-04 16:36:20 +0700

    Build (./script): Adding Command & Script to solve NaughtCoin challenge#15

---
+++ script/15_DeployNaughtCoin.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+import {Script} from "@forge-std/Script.sol";
+import {NaughtCoin} from "@main/15_NaughtCoin.sol";
+// import {ElevatorAttacker} from "@main/11_ElevatorAttacker.sol";
+
+contract DeployNaughtCoinScript is Script {
+    NaughtCoin naughtcoinChallenge;
+
+    function run() public {
+        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        // string memory mnemonic = vm.envString("MNEMONIC");
+
+        // address is already funded with ETH
+        string memory mnemonic ="test test test test test test test test test test test junk";
+        uint256 deployerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+        uint256 attackerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 2); //  address = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+        address attacker = vm.addr(attackerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        naughtcoinChallenge = new NaughtCoin(attacker);
+
+        vm.stopBroadcast();
+    }
+}
